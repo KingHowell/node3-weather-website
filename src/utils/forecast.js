341b556209efaa67bddb4ff11d222e3214fc7126
Querubin <howell.s.querubin@accenture.com>
2022-08-29 03:48:40 +0800

    Add humidity under forecast utils

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,7 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       callback(
         undefined,
-        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. There is a ${body.current.precip}% chance of rain.`
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. There is a ${body.current.precip}% chance of rain. The humidity is ${body.current.humidity}%.`
       );
     }
   });
